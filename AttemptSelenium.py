fec15fccc0083b09b6ffebf83cdbf5537b20b0d7
paddyTea <p.tyrrell2001@gmail.com>
2025-09-14 17:11:30 +1000

    opened window using selenium, begun BS parsing. Currnetly being flagged as a bot in window. Next steps to use 'undetected_chromedriver'

--- AttemptSelenium.py
+++ AttemptSelenium.py
@@ -3,6 +3,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
+from bs4 import BeautifulSoup
+
+
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 
@@ -14,4 +17,8 @@ driver = webdriver.Chrome(options=options)
 #     "prefs", {"profile.managed_default_content_settings.images": 2}
 # )
 
-driver.get("https://www.coles.com.au/")
+driver.get("https://www.coles.com.au/product/coles-no-added-hormone-beef-3-star-regular-mince-1kg-9012825")
+
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+# print (soup.prettify())
