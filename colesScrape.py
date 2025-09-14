72261191bbc9db68e988b2eb903b09c66a37f064
paddyTea <p.tyrrell2001@gmail.com>
2025-09-14 11:06:10 +1000

    parse raw HTML

--- colesScrape.py
+++ colesScrape.py
@@ -1,7 +1,13 @@
 import requests
+from bs4 import BeautifulSoup
 
 response = requests.get('https://www.coles.com.au/product/coles-no-added-hormone-beef-3-star-regular-mince-1kg-9012825')
 
+soup = BeautifulSoup(response.content, 'html.parser')
+
 print(response.status_code)
 
-print(response.text)
+print(soup.prettify())
+
+# print(response.text)
+
