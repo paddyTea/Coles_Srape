1449aaacff6a9c1dd4feface0439d536f86a9e50
paddyTea <p.tyrrell2001@gmail.com>
2025-09-15 18:56:41 +1000

    Logic for extracting nutrition table moved to colesScrape and expanded upon

--- colesScrape.py
+++ colesScrape.py
@@ -13,4 +13,17 @@ except Exception as e:
     print(f"Error loading object: {e}")
 
 soup = HTMLStorage
-print(soup.prettify)
+# print(soup.prettify)
+
+table = soup.find_all('table', {'class': 'sc-9fe53a46-2 heRyOF coles-targeting-TableTableContainer'})[0]
+headers = []
+rows = []
+for i, row in enumerate(table.find_all('tr')):
+    if i == 0:
+        headers.append([el.text.strip() for el in row.find_all('th')])
+    else:
+        rows.append([el.text.strip() for el in row.find_all('th')])
+        rows.append([el.text.strip() for el in row.find_all('td')])
+print(headers)
+print(rows)
+
