87dea0599705103e426b73449d3c4b91296d694a
paddyTea <p.tyrrell2001@gmail.com>
2025-09-15 18:00:39 +1000

    add version 1 logic to collect nutrition table

--- AttemptSelenium.py
+++ AttemptSelenium.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
 from bs4 import BeautifulSoup
-
+import re
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
@@ -20,5 +20,18 @@ driver = webdriver.Chrome(options=options)
 driver.get("https://www.coles.com.au/product/coles-no-added-hormone-beef-3-star-regular-mince-1kg-9012825")
 
 html = driver.page_source
-soup = BeautifulSoup(html, 'html.parser')
+soup = BeautifulSoup(html, 'lxml')
 # print (soup.prettify())
+
+table = soup.find_all('table', {'class': 'sc-9fe53a46-2 heRyOF coles-targeting-TableTableContainer'})[0]
+
+headers = []
+rows = []
+for i, row in enumerate(table.find_all('tr')):
+    if i == 0:
+        headers = [el.text.strip() for el in row.find_all('th')]
+    else:
+        rows.append([el.text.strip() for el in row.find_all('td')])
+
+print(headers)
+print(rows)
