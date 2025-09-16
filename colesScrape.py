b7a43384c8d0979a5b310be2d75c1b197e7d06ac
paddyTea <p.tyrrell2001@gmail.com>
2025-09-16 16:28:13 +1000

    Introduce logic to convert prices to int, convert 1kg and 1L to 100g and 100ml, and introduce weight in 100

--- colesScrape.py
+++ colesScrape.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import pickle
+import re
 
 # page = requests.get(url)
 # soup = BeautifulSoup(page.text, 'html.parser')
@@ -15,20 +16,17 @@ except Exception as e:
 soup = HTMLStorage
 # print(soup.prettify)
 
-
-
-
 # Product Name
 name = soup.find(class_='product__title').text
-print("Name: ",name)
+# print("Name: ",name)
 
 # Product Price
 price = soup.find(class_='price').text
 print("Price: ", price)
 
 # Pricer per
-price_per = soup.find(class_='sc-79c0c972-0 jowmDW').text
-print("Price per: ", price_per)
+str_price_per = soup.find(class_='sc-79c0c972-0 jowmDW').text
+print("Price per: ", str_price_per)
 
 # nutrition table scrape and display
 table = soup.find_all('table', {'class': 'sc-9fe53a46-2 heRyOF coles-targeting-TableTableContainer'})[0]
@@ -42,11 +40,32 @@ for i, row in enumerate(table.find_all('tr')):
         label = [el.text.strip() for el in row.find_all('th')]
         values = [el.text.strip() for el in row.find_all('td')]
         rows.append(label + values)
-print("Nurtrition Information:")
-print(headers)
-for r in rows:
-    print(r)
+# print("Nurtrition Information:")
+# print(headers)
+# for r in rows:
+#     print(r)
 
-print(rows[0][1])
+# print(rows[0][1])
 
 
+ppnumberList = re.findall(r'\d+', str_price_per)
+int_price_per = int(ppnumberList[0])+(int(ppnumberList[1])/100)
+print("String Price Per: ", str_price_per)
+print(ppnumberList)
+print("int_price_per: ", int_price_per)
+
+#Convert from per KG or L to per 100g or 100ml 
+if(ppnumberList[-1]) == '1':
+    int_price_per_100 = float(int_price_per/10)
+print("Price Per 100: ", int_price_per_100)
+
+
+# Int price
+numberList = re.findall(r'\d+', str_price_per)
+int_price = int(numberList[0])+(int(numberList[1])/100)
+print("int_price: ", int_price)
+
+# how many hundreds of grams or ml in the product
+weight_in_100 = int_price/int_price_per_100
+print("weight_in_100: ", weight_in_100)
+print(type(weight_in_100))
