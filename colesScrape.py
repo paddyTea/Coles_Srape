49dcee650e0e640ff513ef13ae5586b5fcd4dee4
paddyTea <p.tyrrell2001@gmail.com>
2025-09-17 18:51:56 +1000

    Create goodsClass, import goods class, create goods methods

--- colesScrape.py
+++ colesScrape.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import pickle
 import re
+from goodsClass import Good
 
 # page = requests.get(url)
 # soup = BeautifulSoup(page.text, 'html.parser')
@@ -14,7 +15,6 @@ except Exception as e:
     print(f"Error loading object: {e}")
 
 soup = HTMLStorage
-# print(soup.prettify)
 
 # Product Name
 name = soup.find(class_='product__title').text
@@ -22,11 +22,11 @@ name = soup.find(class_='product__title').text
 
 # Product Price
 price = soup.find(class_='price').text
-print("Price: ", price)
+# print("Price: ", price)
 
 # Pricer per
-str_price_per = soup.find(class_='sc-79c0c972-0 jowmDW').text
-print("Price per: ", str_price_per)
+price_per = soup.find(class_='sc-79c0c972-0 jowmDW').text
+# print("Price per: ", price_per)
 
 # nutrition table scrape and display
 table = soup.find_all('table', {'class': 'sc-9fe53a46-2 heRyOF coles-targeting-TableTableContainer'})[0]
@@ -40,32 +40,8 @@ for i, row in enumerate(table.find_all('tr')):
         label = [el.text.strip() for el in row.find_all('th')]
         values = [el.text.strip() for el in row.find_all('td')]
         rows.append(label + values)
-# print("Nurtrition Information:")
-# print(headers)
-# for r in rows:
-#     print(r)
 
-# print(rows[0][1])
+instance = Good(name,price,price_per,rows)
+print("Protein :", instance.protein())
+print("Calories: ", instance.calories())
 
-
-ppnumberList = re.findall(r'\d+', str_price_per)
-int_price_per = int(ppnumberList[0])+(int(ppnumberList[1])/100)
-print("String Price Per: ", str_price_per)
-print(ppnumberList)
-print("int_price_per: ", int_price_per)
-
-#Convert from per KG or L to per 100g or 100ml 
-if(ppnumberList[-1]) == '1':
-    int_price_per_100 = float(int_price_per/10)
-print("Price Per 100: ", int_price_per_100)
-
-
-# Int price
-numberList = re.findall(r'\d+', str_price_per)
-int_price = int(numberList[0])+(int(numberList[1])/100)
-print("int_price: ", int_price)
-
-# how many hundreds of grams or ml in the product
-weight_in_100 = int_price/int_price_per_100
-print("weight_in_100: ", weight_in_100)
-print(type(weight_in_100))
---
+++ goodsClass.py
@@ -0,0 +1,42 @@
+import re
+
+class Good:
+    def __init__(self,name,price,price_per_100,nutritionInfo):
+        self.name = name
+        self.price = str_to_float(price)
+        self.price_per_100 = str_to_float(price_per_100)
+        self.weight_in_100s = self.price/self.price_per_100
+        self.nutritionInfo = nutritionInfo
+
+    def type_check(self):
+        print("Price: ",type(self.price), self.price)
+        print("Price Per 100: ", type(self.price_per_100), self.price_per_100)
+        print("Weight Per 100: ", type(self.weight_in_100s), self.weight_in_100s)
+        print("Nutrition Info: ",type(self.nutritionInfo))
+
+    def protein(self):
+        protein = self.nutritionInfo[0][1]
+        numberlist = re.findall(r'\d+', protein)
+        return str_to_float(protein)
+
+    def calories(self):
+        calories = self.nutritionInfo[2][1]
+        numberlist = re.findall(r'\d+', calories)
+        return str_to_float(calories)
+
+    # def protein_per_dollar(self):
+    #     name = self.name
+    #     price = self.price
+    #     protein = self.nutritionInfo[[0][1]]
+
+
+
+def str_to_float(string):
+    numberList = re.findall(r'\d+', string)
+    if len(numberList) == 3:
+        num = float(numberList[0]) + float(numberList[1])/100
+        return num/10
+    elif len(numberList) == 2:
+        return float(numberList[0]) + float(numberList[1])/10
+    else :
+        return float(numberList[0])
